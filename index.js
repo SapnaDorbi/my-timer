3c851576b4d7baafbe3c0d09fa048b482c6b77db
SapnaDorbi <savydorbi@gmail.com>
2021-03-29 00:50:41 +0530

    completed the timer , used animation

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
 
+const perimeter = 2 * Math.PI * circle.getAttribute('r');
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let duration;
 const timer = new Timer(durationInput, startButton, pauseButton, {
-    onStart() {
-        console.log("check onstart");
+    onStart(totalDuration) {
+        duration = totalDuration;
     },
-    onTick() {
-
+    onTick(remainingTime) {
+        circle.setAttribute('stroke-dashoffset',
+        (perimeter * remainingTime) / duration - perimeter
+        );
     },
     onComplete() {
-
+        console.log("complete ...");
     }
 });
-// timer;
-
-
-
--- timer.js
+++ timer.js
@@ -6,6 +6,8 @@ class Timer {
 
         if(callbacks) {
             this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
         }
 
         this.startButton.addEventListener('click', this.start);
@@ -14,10 +16,10 @@ class Timer {
 
     start = () => {
         if(this.onStart){
-            this.onStart();
+            this.onStart(this.remainingTime);
         }
         this.tick();
-        this.interval = setInterval(this.tick, 1000);
+        this.interval = setInterval(this.tick, 50);
     }
 
     tick = () => {
@@ -25,8 +27,14 @@ class Timer {
         // this.durationInput.value = remainingTime - 1;
         if(this.remainingTime <= 0) {
             this.pause();
+            if(this.onComplete) {
+                this.onComplete();
+            }
         } else {
-            this.remainingTime = this.remainingTime - 1;
+            this.remainingTime = this.remainingTime - 0.05;
+            if(this.onTick) {
+                this.onTick(this.remainingTime);
+            }
         }
     }
 
@@ -39,6 +47,6 @@ class Timer {
     }
 
     set remainingTime(time) {
-        return this.durationInput.value = time;
+        return this.durationInput.value = time.toFixed(2);
     }
 }
